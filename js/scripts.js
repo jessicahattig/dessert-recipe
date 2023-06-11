97a0c89625c093b5f7f1fc39041ed115e9d5346f
jessicahattig <jessicahattig@gmail.com>
2023-06-11 16:02:41 -0700

    WIP: have dark and light mode.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,24 @@
 // User Interface Logic
-let h1 = document.querySelector("h1");
-h1.onmouseover = function() {
-  window.alert("I am a heading element.");
-};
+window.onload = function() {
+  let h1 = document.querySelector("h1");
+  h1.onmouseover = function() {
+    window.alert("I am a heading element.");
+  };
 
-let p = document.querySelector("p");
-p.onmouseover = function() {
-  document.querySelector("p>em").innerText = "Don't be surprised";
-};
-
-let img = document.querySelector("img");
-img.onmouseover = function() {
-  img.style.height = "700px";
+  let p = document.querySelector("p");
+  p.onmouseover = function() {
+    document.querySelector("p>em").innerText = "Don't be surprised";
+  };
+  let img = document.querySelector("img");
+  img.onmouseover = function() {
+    img.style.height = "700px";
+  };
+  
+  let dark = document.getElementById("dark");
+  let body = document.querySelector("body");
+  let light = document.getElementById("light");
+    dark.onClick = function() {
+        body.style.backgroundColor= "Black";
+        body.style.textColor= "White";
+    };
 };
